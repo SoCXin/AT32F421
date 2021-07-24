ecc4faf0ead8ad93ac8b9789a1b2d842f335d064
qitas <qitas@qitas.cn>
2021-07-24 19:22:26 +0800

    ADD

---
+++ src/test/flash/User/Bsp/src/bsp_ertc.c
@@ -0,0 +1,163 @@
+/*
+*********************************************************************************************************
+*
+*	程序名称: ERTC驱动模块
+*	文件名称: bsp_ertc.c
+*	版    本: v1.0.0
+*   编    写: 莫利奖
+*	说    明: 
+*
+*	版本记录: 
+*	v1.0.0: 2021年4月9日，初版
+*
+*	Copyright (C), 2021-2031, 深圳市风巽云科技有限公司 https://fxymcu.taobao.com
+*
+*********************************************************************************************************
+*/
+
+#include "bsp.h"
+
+/* 时钟源选择 */
+#define ERTC_CLOCK_SOURCE 1 /* 0: 内部40KHz时钟(有误差) 1: 外部32.768KHz时钟(推荐) */
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_InitHardERTC
+*	功能说明: 初始化实时时钟硬件
+*	形    参: 无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static void bsp_InitHardERTC(void)
+{
+    ERTC_InitType ERTC_InitStructure;
+    ERTC_DateType ERTC_DateStructure;
+    ERTC_TimeType ERTC_TimeStructure;
+    __IO uint8_t ucAsynchPrediv;
+    __IO uint16_t usSynchPrediv;
+
+    /* 使能电源控制器 (PWR) APB1 接口时钟 */
+    RCC_APB1PeriphClockCmd(RCC_APB1PERIPH_PWR, ENABLE);
+    /* 使能对ERTC访问 */
+    PWR_BackupAccessCtrl(ENABLE);
+    /* 重置ERTC域 */
+    RCC_BackupResetCmd(ENABLE);
+    RCC_BackupResetCmd(DISABLE);
+
+#if ERTC_CLOCK_SOURCE == 1 /* 使用外部32.768KHz */
+
+    /* 使能外部32.768KHz时钟源 */
+    RCC_LSEConfig(RCC_LSE_ENABLE);
+    /* 等待时钟源就绪 */
+    while (RCC_GetFlagStatus(RCC_FLAG_LSESTBL) == RESET)
+    {
+    }
+
+    /* 选择ERTC时钟源 */
+    RCC_ERTCCLKConfig(RCC_ERTCCLKSelection_LSE);
+    /* ck_spre(1Hz) = ERTCCLK(LSE) /(ucAsynchPrediv + 1)*(usSynchPrediv + 1)*/
+    ucAsynchPrediv = 127;
+    usSynchPrediv = 255;
+#else /* 内部40KHz时钟 */
+
+    /* 使能内部40KHz时钟源 */
+    RCC_LSICmd(ENABLE);
+    /* 等待时钟源就绪 */
+    while (RCC_GetFlagStatus(RCC_FLAG_LSISTBL) == RESET)
+    {
+    }
+
+    /* 选择ERTC时钟源 */
+    RCC_ERTCCLKConfig(RCC_ERTCCLKSelection_LSI);
+    /* ck_spre(1Hz) = ERTCCLK(LSI) /(ucAsynchPrediv + 1)*(usSynchPrediv + 1)*/
+    ucAsynchPrediv = 127;
+    usSynchPrediv = 319;
+#endif
+
+    /* 使能ERTC时钟 */
+    RCC_ERTCCLKCmd(ENABLE);
+    /* 复位ERTC寄存器 */
+    ERTC_Reset();
+    /* 等待ERTC APB寄存器同步 */
+    ERTC_WaitForSynchro();
+
+    /* 配置ERTC数据寄存器和ERTC预分频器 */
+    ERTC_InitStructure.ERTC_AsynchPrediv = ucAsynchPrediv;   /* 异步预分频 */
+    ERTC_InitStructure.ERTC_SynchPrediv = usSynchPrediv;     /* 同步预分频 */
+    ERTC_InitStructure.ERTC_HourFormat = ERTC_HourFormat_24; /* 小时格式 */
+    ERTC_Init(&ERTC_InitStructure);
+
+    ERTC_ClearFlag(ERTC_FLAG_ALAF);
+
+    /* 设置日期: 2021年4月9日 星期五 */
+    ERTC_DateStructure.ERTC_Year = 0x21;
+    ERTC_DateStructure.ERTC_Month = ERTC_Month_APR;
+    ERTC_DateStructure.ERTC_Date = 0x09;
+    ERTC_DateStructure.ERTC_WeekDay = ERTC_Week_FRI;
+    ERTC_SetDateValue(ERTC_Format_BCD, &ERTC_DateStructure);
+
+    /* 设置时间: 11时50分25秒 */
+    ERTC_TimeStructure.ERTC_AMPM = ERTC_H12_AM; /* 上午 */
+    ERTC_TimeStructure.ERTC_Hours = 0x11;
+    ERTC_TimeStructure.ERTC_Minutes = 0x50;
+    ERTC_TimeStructure.ERTC_Seconds = 0x25;
+    ERTC_SetTimeValue(ERTC_Format_BCD, &ERTC_TimeStructure);
+
+    /* 写标志到备份寄存器 */
+    ERTC_WriteBackupRegister(ERTC_BKP_DT0, 0x1155);
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_InitERTC
+*	功能说明: 初始化ERTC
+*	形    参: 无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+void bsp_InitERTC(void)
+{
+    /* 读取标志 */
+    if (ERTC_ReadBackupRegister(ERTC_BKP_DT0) != 0x1155)
+    {
+        /* 初始化ERTC硬件  */
+        bsp_InitHardERTC();
+        /* 打印时钟 */
+        bsp_ERTC_PrintfTime();
+    }
+    else /* 板子带电复位 */
+    {
+        /* 使能电源控制器 (PWR) APB1 接口时钟 */
+        RCC_APB1PeriphClockCmd(RCC_APB1PERIPH_PWR, ENABLE);
+        /* 使能对ERTC访问 */
+        PWR_BackupAccessCtrl(ENABLE);
+        /* 等待ERTC APB寄存器同步 */
+        ERTC_WaitForSynchro();
+        /* 打印时钟 */
+        bsp_ERTC_PrintfTime();
+    }
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_ERTC_PrintfTime
+*	功能说明: 打印时间
+*	形    参: 无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+void bsp_ERTC_PrintfTime(void)
+{
+    ERTC_DateType ERTC_DateStructure;
+    ERTC_TimeType ERTC_TimeStructure;
+
+    /* 读取日期 */
+    ERTC_GetDateValue(ERTC_Format_BIN, &ERTC_DateStructure);
+    /* 读取日时钟 */
+    ERTC_GetTimeValue(ERTC_Format_BIN, &ERTC_TimeStructure);
+    /* 打印 */
+    printf("->20%0.2d年%0.2d月%0.2d日  %0.2d时%0.2d分%0.2d秒\r\n", ERTC_DateStructure.ERTC_Year, ERTC_DateStructure.ERTC_Month, ERTC_DateStructure.ERTC_Date,
+           ERTC_TimeStructure.ERTC_Hours, ERTC_TimeStructure.ERTC_Minutes, ERTC_TimeStructure.ERTC_Seconds);
+}
+
+/********************************************* End of file **********************************************/
---
+++ src/test/flash/User/main.c
@@ -0,0 +1,145 @@
+﻿/*
+*********************************************************************************************************
+*
+*	程序名称: 片上FLASH读写
+*	文件名称: main.c
+*   MCU型号 : AT32F413RBT7 FLASH:128KB SRAM:64KB
+*   运行平台: 风巽云AT32F421开发板
+*	版    本: v1.0.3
+*   编    写: 莫利奖
+*	说    明: 演示片上FLASH读写
+*
+*	版本记录:
+*	v1.0.0: 2020年11月25日，初版
+*	v1.0.1: 2021年3月3日，升级标准库至1.2.9
+*   v1.0.2: 2021年4月16日，升级标准库至1.3.0，规范示例
+*   v1.0.3: 2021年5月6日，修改读取FLAHS测试函数调用bug
+*
+*	Copyright (C), 2021-2031, 深圳市风巽云科技有限公司 https://fxymcu.taobao.com
+*
+*********************************************************************************************************
+*/
+
+#include "bsp.h"
+
+/* 定义例程名和例程发布日期 */
+#define EXAMPLE_NAME "片上FLASH读写"
+#define EXAMPLE_DATE "2021年5月6日"
+#define DEMO_VER "v1.0.3"
+
+#define USER_FLASH_ADDR 0x0800fc00 /* 最后1K起始位置 */
+
+/* 全局变量 */
+uint8_t ucaWriteBuf[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+uint8_t ucaReadBuf[10] = {0};
+
+/* 函数声明 */
+static void PrintfLogo(void);
+
+/*
+*********************************************************************************************************
+*	函 数 名: main
+*	功能说明: 标准c程序入口
+*	形    参: 无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+int main(void)
+{
+    uint8_t ucKey, ucCnt;
+
+    /* 系统初始化 */
+    System_Init();
+    /* 板子信息 */
+    PrintfLogo();
+    /* 擦擦FLASH */
+    bsp_FLASH_ErasePage(USER_FLASH_ADDR);
+    /* 写入数据 */
+    bsp_FLASH_WriteBuffer(USER_FLASH_ADDR, ucaWriteBuf, sizeof(ucaWriteBuf) / sizeof(uint8_t));
+    /* 读取数据 */
+    bsp_FLASH_ReadBuffer(USER_FLASH_ADDR, ucaReadBuf, sizeof(ucaReadBuf) / sizeof(uint8_t));
+    /* 打印读出的数据 */
+    printf("->FLASH数据 = ");
+    for (ucCnt = 0; ucCnt < sizeof(ucaReadBuf) / sizeof(uint8_t); ucCnt++)
+    {
+        printf("0x%02x ", ucaReadBuf[ucCnt]);
+    }
+    printf("\r\n");
+
+    while (1)
+    {
+        /* 系统10ms时基 */
+        if (ucSys10msFlag)
+        {
+            ucSys10msFlag = 0;
+            /* 按键驱动 */
+            ucKey = bsp_KEY_Driver();
+            if (ucKey == S_KEY)
+            {
+                bsp_LED_Toggle();
+                printf("->按键单击\r\n");
+            }
+            else if (ucKey == L_KEY)
+            {
+                bsp_LED_Off();
+                printf("->按键长按\r\n");
+            }
+        }
+    }
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: PrintfLogo
+*	功能说明: 打印例程名称和例程发布日期
+*	形    参: 无
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+static void PrintfLogo(void)
+{
+    uint32_t CPU_Sn0, CPU_Sn1, CPU_Sn2;
+
+    printf("-------------------------------------------------------------\r\n");
+    printf("->例程名称: %s\r\n", EXAMPLE_NAME); /* 打印例程名称 */
+    printf("->例程版本: %s\r\n", DEMO_VER);     /* 打印例程版本 */
+    printf("->发布日期: %s\r\n", EXAMPLE_DATE); /* 打印例程日期 */
+
+    /* UID */
+    CPU_Sn0 = *(__IO uint32_t *)(0x1FFFF7E8);
+    CPU_Sn1 = *(__IO uint32_t *)(0x1FFFF7E8 + 4);
+    CPU_Sn2 = *(__IO uint32_t *)(0x1FFFF7E8 + 8);
+
+    printf("->CPU: AT32F421C8T7, LQFP48, 主频: %dMHz\r\n", SystemCoreClock / 1000000);
+    printf("->FLASH: 64Kbyte, SRAM: 16Kbyte\r\n");
+    printf("->UID: %08X %08X %08X\n\r", CPU_Sn2, CPU_Sn1, CPU_Sn0);
+
+    /* 库版本 */
+    printf("->库版本: V1.3.0\r\n");
+    printf("-------------------------------------------------------------\r\n");
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: assert_failed
+*	功能说明: 断言函数，宏定义在at32f4xx_conf.h文件
+*	形    参:
+*	          file: 源代码文件名称（__FILE__ 表示源代码文件名）
+*	          line: 代码行号（__LINE__ 表示源代码行号）
+*	返 回 值: 无
+*********************************************************************************************************
+*/
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+    /* 用户可以添加自己的代码报告源代码文件名和代码行号，比如使用串口将错误文件和行号发送至外设 */
+    printf("Wrong parameters value: file %s on line %d\r\n", file, line);
+
+    /* 死循环，便于查找问题 */
+    while (1)
+    {
+    }
+}
+#endif
+
+/********************************************* End of file **********************************************/
